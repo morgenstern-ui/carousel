7d9af66e7fab390f796d9be0c6a9d338c25223c5
teleskop150750 <teleskop150750@gmail.com>
2024-04-16 15:40:14 +0300

    ini

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,9 +1,25 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts({
+    // rollupTypes: true,
+    tsconfigPath: './tsconfig.app.json'
+  })],
+  build: {
+    minify: false,
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+    },
+  },
   test: {
-    globals: true,
     environment: 'jsdom'
   }
 })
