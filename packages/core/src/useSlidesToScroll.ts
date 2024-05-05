0c8e6fefa08e4981cd9c52043b3b75c74ac7d788
teleskop150750 <teleskop150750@gmail.com>
2024-05-05 22:55:56 +0300

    ini

--- packages/core/src/useSlidesToScroll.ts
+++ packages/core/src/useSlidesToScroll.ts
@@ -8,23 +8,21 @@ import { isNumber, mathAbs } from './utils.ts'
  */
 export type SlidesToScrollOptionType = 'auto' | number
 
-/**
- * Тип функции `useSlidesToScroll`.
- */
 export type SlidesToScrollType = ReturnType<typeof useSlidesToScroll>
 
 /**
- * Рассчитывает количество слайдов для прокрутки на основе заданных параметров.
- * @param axis - Тип оси.
- * @param containerSize - Размер области просмотра.
- * @param slidesToScroll - Количество слайдов для прокрутки или `'auto'`.
- * @param loop - Булево значение, указывающее, находится ли карусель в режиме цикла.
- * @param containerRect - Прямоугольник контейнера.
- * @param slideRects - Прямоугольники слайдов.
- * @param startGap - Начальный зазор.
- * @param endGap - Конечный зазор.
- * @param pixelTolerance - Пиксельная допустимая погрешность.
- * @returns Объект с функцией `groupSlides`.
+ * Вычисляет группы слайдов для прокрутки на основе предоставленных параметров.
+ *
+ * @param axis - Конфигурация оси для карусели.
+ * @param containerSize - Размер контейнера карусели.
+ * @param slidesToScroll - Количество слайдов для прокрутки или размер слайдов для прокрутки.
+ * @param loop - Указывает, находится ли карусель в режиме цикла.
+ * @param containerRect - Ограничивающий прямоугольник контейнера карусели.
+ * @param slideRects - Ограничивающие прямоугольники слайдов.
+ * @param startGap - Начальный зазор между контейнером и первым слайдом.
+ * @param endGap - Конечный зазор между контейнером и последним слайдом.
+ * @param pixelTolerance - Допустимое отклонение в пикселях для определения размеров групп.
+ * @returns Объект с функцией `groupSlides`, которая вычисляет группы слайдов для прокрутки.
  */
 export function useSlidesToScroll(
   axis: AxisType,
@@ -41,9 +39,22 @@ export function useSlidesToScroll(
   const groupByNumber = isNumber(slidesToScroll)
 
   /**
-   * Группирует массив элементов по заданному числу.
+   * Группирует массив элементов на основе опции `slidesToScroll`.
+   * Если `slidesToScroll` равно `'auto'`, то группирует элементы на основе размеров слайдов.
+   * Если `slidesToScroll` равно числу, то группирует элементы на основе этого числа.
+   * 
    * @param array - Массив для группировки.
-   * @param groupSize - Размер каждой группы.
+   * @returns Массив сгруппированных элементов.
+   */
+  function groupSlides<Type>(array: Type[]): Type[][] {
+    return groupByNumber ? byNumber(array, slidesToScroll) : bySize(array)
+  }
+
+  /**
+   * Группирует массив элементов по заданному числу.
+   *
+   * @param array - Массив для группировки.
+   * @param groupSize - Размер группы.
    * @returns Массив сгруппированных элементов.
    */
   function byNumber<Type>(array: Type[], groupSize: number): Type[][] {
@@ -58,7 +69,8 @@ export function useSlidesToScroll(
   }
 
   /**
-   * Группирует массив элементов по их размеру.
+   * Группирует массив элементов на основе размера слайдов.
+   *
    * @param array - Массив для группировки.
    * @returns Массив сгруппированных элементов.
    */
@@ -98,14 +110,6 @@ export function useSlidesToScroll(
     return groups
   }
 
-  /**
-   * Группирует массив элементов на основе опции `slidesToScroll`.
-   * @param array - Массив для группировки.
-   * @returns Массив сгруппированных элементов.
-   */
-  function groupSlides<Type>(array: Type[]): Type[][] {
-    return groupByNumber ? byNumber(array, slidesToScroll) : bySize(array)
-  }
 
   const self = {
     groupSlides
